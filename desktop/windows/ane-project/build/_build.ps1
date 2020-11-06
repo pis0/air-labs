2db6e041e08537e04bfac48c9e28a07cd1d62097
pis0 <msdraco@gmail.com>
2020-11-06 17:30:26 -0300

    ane project improvements

---
+++ desktop/windows/ane-project/build/_build.ps1
@@ -0,0 +1,2 @@
+C:\workspace\_airSDK\bin\adt.bat -package -target ane com.assukar.desktop.windows.WindowsExtension.ane WindowsExtensionWrapper-extension.xml -swc lib.swc -platform Windows-x86 library.swf dll-project.dll
+pause
