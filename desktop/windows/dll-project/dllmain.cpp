b06b4652ca1af414fe881ddcf7aed2ec6445e6d4
pis0 <msdraco@gmail.com>
2020-11-09 17:49:37 -0300

    dll project labs

--- desktop/windows/dll-project/dllmain.cpp
+++ desktop/windows/dll-project/dllmain.cpp
@@ -4,9 +4,70 @@
 #include <string>
 #include <assert.h>
 #include <sstream> 
+#include <intrin.h>
+
+void cpuid(uint32_t op, uint32_t& eax, uint32_t& ebx, uint32_t& ecx, uint32_t& edx)
+{
+	int regs[4];
+	__cpuid(regs, op);
+	eax = regs[0];
+	ebx = regs[1];
+	ecx = regs[2];
+	edx = regs[3];
+}
+enum CpuidFeatures
+{
+	PSN = 1 << 18, 
+};
+
+// TODO to fix
+uint32_t cpuid_features()
+{
+	uint32_t eax, ebx, ecx, edx;
+	cpuid(PSN, eax, ebx, ecx, edx);
+
+	// byte swap
+	int first = ((eax >> 24) & 0xff) | ((eax << 8) & 0xff0000) | ((eax >> 8) & 0xff00) | ((eax << 24) & 0xff000000);
+	int last = ((edx >> 24) & 0xff) | ((edx << 8) & 0xff0000) | ((edx >> 8) & 0xff00) | ((edx << 24) & 0xff000000);
+
+	std::stringstream buffer;
+
+	scanf_s("%08X%08X", first, last);
+
+	return edx;
+}
+std::string getProcessorSerialNumber()
+{
+	std::stringstream buffer;
+	buffer << cpuid_features() << std::endl;
+	return buffer.str();
+}
+
+
+std::string getVolumeSerialNumber()
+{
+	DWORD volumeSerialNumber = NULL;
+	if (GetVolumeInformationA(
+		NULL,
+		NULL,
+		NULL,
+		&volumeSerialNumber,
+		NULL,
+		NULL,
+		NULL,
+		NULL
+	) == 0)
+	{
+		return "N/A";
+	}
+
+	std::string stringVolumeSerialNumber = std::to_string(volumeSerialNumber);
+	return stringVolumeSerialNumber;
+}
 
 FREObject ASPassAString(FREContext ctx, void* funcData, uint32_t argc, FREObject argv[]) 
 {
+
 	// What this function does: reads a string passed in from ActionScript,
 	// outputs it to the console, then sends it back to ActionScript.
 
@@ -38,16 +99,22 @@ FREObject ASPassAString(FREContext ctx, void* funcData, uint32_t argc, FREObject
 
 	if ((FRE_OK == status) && (0 < strLength) && (NULL != nativeCharArray))
 	{
+		/*
 		// Read the characters into a c string... 
 		std::string nativeString((const char*)nativeCharArray);
-
 		// ...and output it into the console to see what we received:
 		std::stringstream  stros;
 		stros << "This is the string we received from ActionScript: ";
 		stros << nativeString;
-
 		// Now let's put the characters back into a FREObject...
 		FRENewObjectFromUTF8(strLength, nativeCharArray, &retObj);
+		*/
+
+		// TODO to review 
+		std::string machineGuid = getProcessorSerialNumber(); // getVolumeSerialNumber(); 
+		const uint8_t* result = reinterpret_cast<const uint8_t*>(machineGuid.c_str()); 
+		FRENewObjectFromUTF8(machineGuid.size(), result, &retObj);
+
 	}
 
 	// ... and send them back to ActionScript:
@@ -63,7 +130,7 @@ void contextFinalizer(FREContext ctx)
 void contextInitializer(
 	void* extData,
 	const uint8_t* ctxType,
-	FREContext				   ctx,
+	FREContext ctx,
 	uint32_t* numFunctionsToSet,
 	const FRENamedFunction** functionsToSet)
 {
@@ -100,20 +167,3 @@ extern "C"
 	}
 }
 
-/*
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved 
-                     )
-{
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE; 
-}
-*/
